58f0e8509db590f242cd581f40ea469876e8d2cb
Charles Hogg <charles.r.hogg@gmail.com>
2014-07-10 14:27:39 -0400

    Factor out code and add ESG animation

--- make_gifs.R
+++ make_gifs.R
@@ -16,27 +16,46 @@ source('./interp.R')
 M.aniso$SetNoiseBounds(sigma.vals=2e-6)
 m.post <- M.aniso$PosteriorMean(d=d.strain, X.out=x.grid)
 L.strain <- M.aniso$L(d=d.strain, X.out=x.grid)
-width <- 1000  # pixels
-height <- 600  # pixels
-n.frames <- 200
-time.mat <- (L.strain %*% BubblingRandomMatrix(
-    n.pts=nrow(x.grid), N=N.frames, n.times=n.frames))
-time.mat <- time.mat + as.vector(m.post)
+
+# Function to create a single steel strain animation.
+SteelStrainAnimation <- function(random.matrix, label) {
+  width <- 1000  # pixels
+  height <- 600  # pixels
+  anim.dir <- sprintf('custom/steel_strain/%s', label)
+  dir.create(anim.dir, recursive=TRUE)
+
+  time.mat <- (L.strain %*% random.matrix) + as.vector(m.post)
+  open3d()
+  par3d(windowRect = 50 + c(0, 0, width, height))
+  view3d(theta=45, phi=25, zoom=0.4)
+  N.frames <- ncol(random.matrix)
+  for (i in 1:N.frames) {
+    d.strain$Plot2D(dist.factor=0.15, max.points=Inf, Y.scale=500, clear=TRUE)
+    d.gap$Plot2D(dist.factor=0.15, max.points=Inf, Y.scale=500, clear=FALSE,
+                 color='blue')
+    PlotSurface(X=x.grid, Y=time.mat[, i])
+    rgl.snapshot(filename=sprintf('%s/steel_strain_%04d.png', anim.dir, i),
+      top=TRUE)
+  }
+  rgl.close()
+}
+
+# Get the times straight.
+N.frames <- 100
+N.keyframes <- 8
+N.smoothseeds <- N.keyframes / 2
+total.time <- N.keyframes
+dt <- total.time / N.frames
+t.out <- seq(from=dt, to=total.time, by=dt)
+
+# Construct the matrices.
+N.points <- ncol(L.strain)
+seed.matrix <- matrix(rnorm(n=N.keyframes * N.points), nrow=N.keyframes)
 
 # Construct the animations.
-anim.dir <- 'custom/steel_strain/plain'
-dir.create(anim.dir, recursive=TRUE)
-open3d()
-par3d(windowRect = 50 + c(0, 0, width, height))
-view3d(theta=45, phi=25, zoom=0.4)
-for (i in 1:n.frames) {
-  d.strain$Plot2D(dist.factor=0.15, max.points=Inf, Y.scale=500, clear=TRUE)
-  d.gap$Plot2D(dist.factor=0.15, max.points=Inf, Y.scale=500, clear=FALSE,
-               color='blue')
-  PlotSurface(X=x.grid, Y=time.mat[, i])
-  rgl.snapshot(filename=sprintf('%s/steel_strain_%04d.png', anim.dir, i),
-    top=TRUE)
-}
-rgl.close()
+SteelStrainAnimation(
+    t(InterpESG(t.out=t.out, N.frames=N.keyframes) %*% seed.matrix), 'esg')
+SteelStrainAnimation(
+    t(MyMatrix(t=t.out, N=N.smoothseeds) %*% seed.matrix), 'smooth')
 
 cat("done!\n")
